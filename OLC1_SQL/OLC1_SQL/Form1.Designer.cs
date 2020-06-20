c1575c9bf972f1f18ba174bfc726cc19ac5c5d6d
Alex4191 <53594159+Alex4191@users.noreply.github.com>
2020-06-20 01:24:27 -0600

    'scanner add'

---
+++ OLC1_SQL/OLC1_SQL/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace OLC1_SQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analyzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadTablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewTablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showTokensToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showDTreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userGuideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.technicalGuideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1253, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.analyzeToolStripMenuItem,
+            this.loadTablesToolStripMenuItem,
+            this.viewTablesToolStripMenuItem,
+            this.showTokensToolStripMenuItem,
+            this.showDTreeToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // analyzeToolStripMenuItem
+            // 
+            this.analyzeToolStripMenuItem.Name = "analyzeToolStripMenuItem";
+            this.analyzeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.analyzeToolStripMenuItem.Text = "Analyze";
+            this.analyzeToolStripMenuItem.Click += new System.EventHandler(this.analyzeToolStripMenuItem_Click);
+            // 
+            // loadTablesToolStripMenuItem
+            // 
+            this.loadTablesToolStripMenuItem.Name = "loadTablesToolStripMenuItem";
+            this.loadTablesToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.loadTablesToolStripMenuItem.Text = "Load Tables";
+            this.loadTablesToolStripMenuItem.Click += new System.EventHandler(this.loadTablesToolStripMenuItem_Click);
+            // 
+            // viewTablesToolStripMenuItem
+            // 
+            this.viewTablesToolStripMenuItem.Name = "viewTablesToolStripMenuItem";
+            this.viewTablesToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.viewTablesToolStripMenuItem.Text = "View Tables";
+            // 
+            // showTokensToolStripMenuItem
+            // 
+            this.showTokensToolStripMenuItem.Name = "showTokensToolStripMenuItem";
+            this.showTokensToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.showTokensToolStripMenuItem.Text = "Show Tokens";
+            // 
+            // showDTreeToolStripMenuItem
+            // 
+            this.showDTreeToolStripMenuItem.Name = "showDTreeToolStripMenuItem";
+            this.showDTreeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.showDTreeToolStripMenuItem.Text = "Show D Tree";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.userGuideToolStripMenuItem,
+            this.technicalGuideToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // userGuideToolStripMenuItem
+            // 
+            this.userGuideToolStripMenuItem.Name = "userGuideToolStripMenuItem";
+            this.userGuideToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.userGuideToolStripMenuItem.Text = "User Guide";
+            // 
+            // technicalGuideToolStripMenuItem
+            // 
+            this.technicalGuideToolStripMenuItem.Name = "technicalGuideToolStripMenuItem";
+            this.technicalGuideToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.technicalGuideToolStripMenuItem.Text = "Technical Guide";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1047, 596);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1253, 699);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "OLC1 SQL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userGuideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem technicalGuideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analyzeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadTablesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewTablesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showTokensToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showDTreeToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ OLC1_SQL/OLC1_SQL/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OLC1_SQL
+{
+    public partial class Form1 : Form
+    {
+        private List<Paths> List_Paths;
+        private String actual_path;
+        private String actual_name;
+        Scanner_201602880 s;
+        Parser_201602880 p;
+        public Form1()
+        {
+            InitializeComponent();
+            List_Paths = new List<Paths>();
+            s= new Scanner_201602880();
+            p = new Parser_201602880();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "[OLC1]SQLe|*.sqle";
+            string row = "";
+            string text = "";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                string path1 = openFile.FileName;
+                StreamReader strReader = new StreamReader(path1, System.Text.Encoding.UTF8);
+                string nameC = Path.GetFileNameWithoutExtension(openFile.FileName);
+                while ((row = strReader.ReadLine()) != null)
+                {
+                    text += row + System.Environment.NewLine;
+                }
+                richTextBox1.Text = text;
+                strReader.Close();
+                List_Paths.Clear();
+                List_Paths.Add(new Paths(path1, nameC));
+
+                actual_path = path1;
+                actual_name = nameC;
+                this.Text = actual_name;
+
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Boolean exists = false;
+            string path = "";
+            for (int i = 0; i < List_Paths.Count; i++)
+            {
+                Paths pt = List_Paths.ElementAt(i);
+                if (actual_path == pt.getPath())
+                {
+                    path = actual_path;
+                    exists = true;
+                }
+            }
+            if (exists == false)
+            {
+                save_As();
+            }
+            else
+            {
+                save(path);
+            }
+        }
+        private void save(string path)
+        {
+            try
+            {
+
+                string text = richTextBox1.Text;
+                StreamWriter writer = new StreamWriter(path);
+                writer.Write(text);
+                writer.Flush();
+                writer.Close();
+
+                string name = Path.GetFileNameWithoutExtension(path);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    
+        private void save_As()
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "[OLC1]SQLe|*.sqle";
+            saveFile.Title = "Save File As";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = (FileStream)saveFile.OpenFile();
+                fs.Close();
+                string path = saveFile.FileName;
+                save(path);
+                string nombre = Path.GetFileNameWithoutExtension(path);
+                List_Paths.Add(new Paths(path, nombre));
+                actual_path = path;
+                actual_name = nombre;
+                this.Text = actual_name;
+
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save_As();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Develop By: Bryan Alvarado"+"\nID: 201602880","Information");
+        }
+
+        private void analyzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String data = richTextBox1.Text;
+            
+            s.Analyze(data);
+            p.Parser(s.TokensArray);
+            if (s.Errors.Count != 0)
+
+            {
+                s.Html_Errores();
+                s.Html_Tokens();
+            }
+            else
+            {
+                s.Html_Tokens();
+            }
+           
+        }
+
+        private void loadTablesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
